1fb92048efc1402e20c800047106de6d87d92a5a
maumi <mauismini@Mariuss-Mini.lan>
2014-11-02 17:11:50 +0100

    Update throttler.sh
    * Cleaning the code and just keeping the necessary lines.
    * Adding code sections to make the code more clear

--- throttler.sh
+++ throttler.sh
@@ -1,50 +1,36 @@
 #!/bin/bash
-
-#You can replace HandBrake with any Program you like 
-# but look for case sensitivity
-
-hbpid=$(pgrep HandBrake)
+#-------[ File Description ]-------#
+# You can replace HandBrake with any Program you like 
+# but look for case sensitivity.
 
 echo "This Script must be in the same directory like cputhrottle and 
       be running with sudo. Alternatively you can add a path at the program call.
       If you want to end the script just make twice ctrl+c."
 
-if [ -z "$hbpid" ]
-then
-     echo "No HandBrake running! Exit!"
-     exit 1
-fi
-
-thrpid=$(pgrep cputhrottle)
-#echo $thrpid
-if [ -n "$thrpid" ]
-then
-    echo "CpuThrottle already running. Exit"
-    exit 2
-fi
-
-#Here you can set your cpuPercentage Value you like
+#-------[ Configuration ]-------#
+# Here you can set your cpuPercentage Value you like
 cpuPerc=600
 
+#-------[ Main script ]-------#
+hbpid=$(pgrep HandBrake)
 while true
 do
-     if [ -z "$hbpid" ]
-     then
-        echo "No HandBrake running! Exit!"
-        exit 1
-     fi
-
-     thrpid=$(pgrep cputhrottle)
-     #echo $thrpid
-     if [ -z "$thrpid" ]
-     then
-
+	if [ -z "$hbpid" ]
+	then
+     	echo "No HandBrake running! Exit!"
+     	exit 1
+	fi
+    thrpid=$(pgrep cputhrottle)
+    if [ -z "$thrpid" ]
+    then
         echo "Starting cputhrottle..."
         date
         set -e
         ./cputhrottle $hbpid $cpuPerc || exit 3
         set +e
-     fi
+    else
+    	echo "Cputhrottle already running. Exit!"
+    fi
    
-     sleep 5
+    sleep 5
 done 
